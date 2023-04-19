a1dc4a94d9802f8916d49b2636da9e22e81bcb40
so-ga <natto.and.eggs+0@gmail.com>
2023-04-19 13:36:35 +0900

    fix

--- src/lib/fire-document.ts
+++ src/lib/fire-document.ts
@@ -9,8 +9,6 @@ import {
 
 type Collection<TData> = { ref: CollectionReference<TData> };
 
-type Snapshot<TData> = Pick<DocumentSnapshot<TData>, 'id' | 'ref' | 'data'>;
-
 type Constructor<T, TData> = { new (id: string, ref: DocumentReference<TData>, data: TData): T };
 
 type FireDocumentObject<TData> = { id: string; ref: DocumentReference<TData> } & TData;
@@ -28,7 +26,10 @@ export class FireDocument<TData> {
     return new this(docRef.id, docRef, data);
   }
 
-  static fromSnapshot<T, TData>(this: Constructor<T, TData>, snapshot: Snapshot<TData>) {
+  static fromSnapshot<T, TData>(
+    this: Constructor<T, TData>,
+    snapshot: Pick<DocumentSnapshot<TData>, 'id' | 'ref' | 'data'>
+  ) {
     return new this(snapshot.id, snapshot.ref, snapshot.data() as TData);
   }
 
